13a280ffef934b42eafdec593c555f3fed4d9141
Marrow-SG <2242205@talnet.nl>
2025-11-20 11:22:35 +0100

    I don't remember
    I believe these are changes made during the previous class on Tuesday of the second to last week in November.
    
    I remember that it was about player input, so this code is all related to movement and getting it to work. But last I remembered, the code stopped Unity from starting up the game, so it might be bad or unupdated code.
    
    If everything is right with it though, this is just code that's indirectly copied from the lesson to get the character to move around, I'll change it later, maybe try to use RigidBody2D to allow for player input instead.

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.rotation = Quaternion.Euler(0, 0, 0);
+        //transform.localScale = new Vector3(0,0,0);
+
+
+
+        if (Input.GetKey(KeyCode.W)) {
+            print("I pressed W!");
+            transform.position += new Vector3(0, 1, 0) * (Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("I pressed A!");
+            transform.position -= new Vector3(1, 0, 0) * (Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("I pressed S!");
+            transform.position -= new Vector3(0, 1, 0) * (Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("I pressed D!");
+            transform.position += new Vector3(1, 0, 0) * (Time.deltaTime * _speed);
+        }
+
+
+    }
+}
